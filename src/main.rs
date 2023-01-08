338ab4442b32fff743d699b8f1908026575b4ace
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-01-08 08:44:23 +0300

    added the JoinHandle type to ensure thread completes running before sleep

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::thread;
+use std::thread::{self};
 use std::time::Duration;
 
+
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +14,6 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+    
+    handle.join().unwrap();
 }
